e3876457f9dd232068ad2b156b2de2f7f6ef6191
RodrigoMaia03 <rodrigonaia2003@gmail.com>
2024-12-04 21:47:58 -0300

    Alteração no código do chat bot

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,6 @@
 from nio import AsyncClient, RoomMessageText
 
-# Configura o cliente
+# Substitua "SEU_ACCESS_TOKEN" pelo token da sua conta do bot
 client = AsyncClient("https://matrix.org", "syt_Y2hhdGJvdHBpYmlj_iSNxEhejwAcjharwAQnw_4NsS2D")
 
 # Função de resposta básica
@@ -15,7 +15,6 @@ async def message_callback(room, event):
 # Conecta e ouve eventos
 async def main():
     client.add_event_callback(message_callback, RoomMessageText)
-    await client.login()
     await client.sync_forever(timeout=30000)  # Mantém sincronizado
 
 import asyncio
